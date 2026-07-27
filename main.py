5db2f119ce6946f379b4d43f4b2409802a0cc980
David Young <thedewi@users.noreply.github.com>
2026-07-27 12:22:46 +1000

    Support reading input from stdin

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import argparse
 from pathlib import Path
+import sys
 
 from pyfiglet import figlet_format
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Render a file's contents as ASCII art."
+    parser = argparse.ArgumentParser(description="Render text as ASCII art.")
+    parser.add_argument(
+        "file", nargs="?", help="path to the file to render (default: stdin)"
     )
-    parser.add_argument("file", help="path to the file to render")
     args = parser.parse_args()
 
-    print(figlet_format(Path(args.file).read_text()), end="")
+    if args.file is None:
+        text = sys.stdin.read()
+    else:
+        text = Path(args.file).read_text()
+
+    print(figlet_format(text), end="")
 
 
 if __name__ == "__main__":
